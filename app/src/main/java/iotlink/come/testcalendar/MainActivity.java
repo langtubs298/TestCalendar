5b30b600f3ffee5875cefdca4872b89973f93510
langtubs298 <luongvien.binhson@gmail.com>
2018-07-26 21:41:03 +0700

    2

--- app/src/main/java/iotlink/come/testcalendar/MainActivity.java
+++ app/src/main/java/iotlink/come/testcalendar/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "t4", Toast.LENGTH_LONG).show();
         //change somethin3
         //asdaaa
-        //6
         //c1
     }
 }
