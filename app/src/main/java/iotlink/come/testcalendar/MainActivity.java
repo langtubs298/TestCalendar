432dcf972af4bfcbef5b41f7b272c1c7f9701f50
langtubs298 <luongvien.binhson@gmail.com>
2018-07-29 20:58:19 +0700

    2

--- app/src/main/java/iotlink/come/testcalendar/MainActivity.java
+++ app/src/main/java/iotlink/come/testcalendar/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "t4", Toast.LENGTH_LONG).show();
         //f
+        //ff
     }
 }
